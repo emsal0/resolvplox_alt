d4d3d2635ab8e953b0830079e7e999414c593e68
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2016-07-03 00:33:10 -0700

    redefined query generation so it is actually the correct number of bits/other formatting things. also docs for message struct

--- dns_messages/message.go
+++ dns_messages/message.go
@@ -1,5 +1,8 @@
 package dns_messages
 
+// This is a struct that represents a DNS message
+// head, question, answers, authority, and additional sections are byte strings that
+// are specified according to RFC 1035.
 type Message struct {
 	head       []byte
 	question   []byte
@@ -8,6 +11,7 @@ type Message struct {
 	additional []byte
 }
 
+// Turns the DNS message into a byte slice for sending
 func (msg *Message) ToByteSlice() []byte {
 	all_components := [][]byte{msg.head, msg.question, msg.answers, msg.authority, msg.additional}
 	rlt := []byte{}
