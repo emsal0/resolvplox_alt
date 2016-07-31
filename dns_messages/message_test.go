026c0344a17105935a68fe54775243be5f36d0d7
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2016-07-30 22:32:31 -0700

    Added unit tests, changed scope of dns messages struct fields (to public)

--- dns_messages/message_test.go
+++ dns_messages/message_test.go
@@ -1,7 +1,44 @@
 package dns_messages_test
 
-import "testing"
+import (
+	"github.com/emsal1863/resolvplox_alt/dns_messages"
+	"testing"
+)
+
+func testEq(a, b []byte) bool {
+
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func TestToByteSlice(t *testing.T) {
-	t.Error("Can't commit a test that hasn't failed yet!")
+	msg := dns_messages.Message{
+		Head:       []byte{'a'},
+		Question:   []byte{'b'},
+		Answers:    []byte{'c'},
+		Authority:  []byte{'d'},
+		Additional: []byte{'e'}}
+
+	msgConverted := msg.ToByteSlice()
+
+	if !testEq(msgConverted, []byte{'a', 'b', 'c', 'd', 'e'}) {
+		t.Error("ToByteSlice didn't append all fields in message struct together. Converted: " + string(msgConverted) + ", Expected: abcde")
+	}
 }
