64f0d3b6bc3d8f7c69a97920bb5c4aa2b1eae35b
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2016-08-02 01:20:00 -0700

    Added some basic UDP server functionality with concurrency. Also added integration tests (still need to figure out how to separate unit and integration tests).

--- networking/networking_logic.go
+++ networking/networking_logic.go
@@ -31,3 +31,29 @@ func SendQuery(msg dns_messages.Message, dns_server string) (response []byte, nu
 
 	return response, numBytes, err
 }
+
+// Sends a DNS Message in byte slice form to the desired server (only port 53 supported right now)
+func SendQueryByteSlice(msg []byte, dns_server string) (response []byte, numBytes int, err error) {
+	serverAddr, err := net.ResolveUDPAddr("udp", dns_server+":53")
+	if err != nil {
+		return nil, -1, err
+	}
+
+	localAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+	if err != nil {
+		return nil, -1, err
+	}
+
+	conn, err := net.DialUDP("udp", localAddr, serverAddr)
+	if err != nil {
+		return nil, -1, err
+	}
+	response = make([]byte, 1024)
+	conn.Write(msg)
+	numBytes, _, err = conn.ReadFromUDP(response)
+
+	err = nil
+	conn.Close()
+
+	return response, numBytes, err
+} //TODO -- make this a simple hook to the SendQuery method (needs dns_messages.FromByteSlice for validation)
