f8aad0bbdcd4f2aa181b74b9982ff3a80f9efd98
AbsoluteZero2A03 <AbsoluteZero2A03@gmail.com>
2016-08-18 07:46:45 -0700

    Added functionality for questions as struct, converting from byte slice to dns message struct

--- dns_messages/message_test.go
+++ dns_messages/message_test.go
@@ -30,37 +30,42 @@ func testEq(a, b []byte) bool {
 
 func TestToByteSlice(t *testing.T) {
 	msg := dns_messages.Message{
-		Head:       []byte{'a'},
-		Question:   []byte{'b'},
+		Head: []byte{'a'},
+		Questions: []dns_messages.Question{dns_messages.Question{
+			Name:  []byte{'b'},
+			Type:  []byte{0x00},
+			Class: []byte{0x00},
+		}},
 		Answers:    []byte{'c'},
 		Authority:  []byte{'d'},
 		Additional: []byte{'e'}}
 
 	msgConverted := msg.ToByteSlice()
 
-	if !testEq(msgConverted, []byte{'a', 'b', 'c', 'd', 'e'}) {
+	if !testEq(msgConverted, []byte{'a', 'b', 0x00, 0x00, 'c', 'd', 'e'}) {
 		t.Error("ToByteSlice didn't append all fields in message struct together. Converted: " + string(msgConverted) + ", Expected: abcde")
 	}
 }
 
 func TestNametoQuery(t *testing.T) {
-    id, msg := dns_messages.NametoQuery([]byte("www.google.com"))
-    expectedQueryAfterId := []byte{
-        0x01, 0x00,
-        0x00, 0x01,
-        0x00, 0x00,
-        0x00, 0x00,
-        0x00, 0x00,
-        3, 'w', 'w', 'w',
-        6, 'g', 'o', 'o', 'g', 'l', 'e',
-        3, 'c', 'o', 'm',
-        0x00,
-        0x00, 0x01, // qtype
-        0x00, 0x01, // qclass
-    }
-    expectedQuery := append(id, expectedQueryAfterId...)
-
-    if !testEq(msg.ToByteSlice(), expectedQuery) {
-        t.Error("NameToQuery didn't construct expected query for www.google.com")
-    }
+	id, msg := dns_messages.NametoQuery([]byte("www.google.com"))
+	expectedQueryAfterId := []byte{
+		0x01, 0x00,
+		0x00, 0x01,
+		0x00, 0x00,
+		0x00, 0x00,
+		0x00, 0x00,
+		3, 'w', 'w', 'w',
+		6, 'g', 'o', 'o', 'g', 'l', 'e',
+		3, 'c', 'o', 'm',
+		0x00,
+		0x00, 0x01, // qtype
+		0x00, 0x01, // qclass
+	}
+	expectedQuery := append(id, expectedQueryAfterId...)
+	actualQuery := msg.ToByteSlice()
+	if !testEq(actualQuery, expectedQuery) {
+		t.Log(actualQuery)
+		t.Error("NameToQuery didn't construct expected query for www.google.com")
+	}
 }
