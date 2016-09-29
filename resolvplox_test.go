16ba6edce545c347fc2cb315bd710294c309f381
Em Sal <absolutezero2a03@gmail.com>
2016-09-29 09:52:10 -0700

    added new test, changed example config

--- resolvplox_test.go
+++ resolvplox_test.go
@@ -43,3 +43,40 @@ func TestResolvploxServer(t *testing.T) {
 
 	t.Log(response)
 }
+
+// func TestResolvploxServer2(t *testing.T) {
+// 	testQuery := []byte{
+// 		0x04, 0x9b,
+// 		0x01, 0x00,
+// 		0x00, 0x01,
+// 		0x00, 0x00,
+// 		0x00, 0x00,
+// 		0x00, 0x00,
+// 		6, 'g', 'o', 'o', 'g', 'l', 'e',
+// 		3, 'c', 'o', 'm',
+// 		0x00,
+// 		0x00, 0x01, // qtype
+// 		0x00, 0x01, // qclass
+// 	}
+//
+// 	remote_addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:20841")
+// 	local_addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:0")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+//
+// 	conn, err := net.DialUDP("udp", local_addr, remote_addr)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	response := make([]byte, 1024)
+// 	conn.Write(testQuery)
+//
+// 	numBytes, _, err := conn.ReadFromUDP(response)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	response = response[:numBytes]
+//
+// 	t.Log(response)
+// }
